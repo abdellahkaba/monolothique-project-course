88cf58c7b3453a93b32e13aaef36d7fbf06fc277
Abdellah <abdallahkaba98@gmail.com>
2024-08-31 22:27:04 +0000

    init-configuration

---
+++ book-network/src/main/java/com/isi/book/BookNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.isi.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApiApplication.class, args);
+	}
+
+}
