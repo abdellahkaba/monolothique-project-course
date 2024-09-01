8d5ffd58e19d8ebeb86123d50493086cb5872f1b
Abdellah <abdallahkaba98@gmail.com>
2024-09-01 17:59:00 +0000

    class-for-AuditUserAuthentifie

--- book-network/src/main/java/com/isi/book/config/BeansConfig.java
+++ book-network/src/main/java/com/isi/book/config/BeansConfig.java
@@ -4,6 +4,7 @@ package com.isi.book.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -34,5 +35,10 @@ public class BeansConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+        return new ApplicationAuditAware();
     }
 }
