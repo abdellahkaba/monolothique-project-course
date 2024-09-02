5d7af4a5a61d75a20754e5b15389b00fa2d02ade
Abdellah <abdallahkaba98@gmail.com>
2024-09-02 23:54:48 +0000

    generate-http-openapi-documentation

---
+++ book-network-frontend/src/app/services/models/book-response.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export interface BookResponse {
+  archived?: boolean;
+  authorName?: string;
+  cover?: string;
+  id?: number;
+  isbn?: string;
+  owner?: string;
+  rate?: number;
+  shareable?: boolean;
+  synopsis?: string;
+  title?: string;
+}
