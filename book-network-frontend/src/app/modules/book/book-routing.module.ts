50966cdbe74515bb30bd39bbadb5a94b8377d4a6
Abdellah <abdallahkaba98@gmail.com>
2024-09-03 16:12:20 +0000

    implement-book-list-component-and-page-home

--- book-network-frontend/src/app/modules/book/book-routing.module.ts
+++ book-network-frontend/src/app/modules/book/book-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainComponent} from "./pages/main/main.component";
+import {BookListComponent} from "./pages/book-list/book-list.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: MainComponent
-  }
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: BookListComponent
+      }
+    ]
+  },
+
 ];
 
 @NgModule({
---
+++ book-network-frontend/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit} from '@angular/core';
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
+import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-book-list',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './book-list.component.html',
+  styleUrl: './book-list.component.scss'
+})
+export class BookListComponent implements OnInit{
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+
+  constructor(
+    private bookService: BookService,
+    private router: Router
+  ) {
+  }
+  ngOnInit(): void {
+    this.findAllBooks()
+  }
+
+  private findAllBooks() {
+    this.bookService.findAllBooks({
+      page: this.page,
+      size: this.size
+    })
+      .subscribe({
+        next: (books) => {
+          this.bookResponse = books;
+        }
+      });
+  }
+
+}
