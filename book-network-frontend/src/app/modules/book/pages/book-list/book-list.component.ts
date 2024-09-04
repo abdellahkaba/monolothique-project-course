f03b8977bc3bab25d35016772c9f62048171f77c
Abdellah <abdallahkaba98@gmail.com>
2024-09-04 20:00:27 +0000

    implemente-the-borrow-action

--- book-network-frontend/src/app/modules/book/pages/book-list/book-list.component.ts
+++ book-network-frontend/src/app/modules/book/pages/book-list/book-list.component.ts
@@ -2,15 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {BookService} from "../../../../services/services/book.service";
 import {Router} from "@angular/router";
 import {PageResponseBookResponse} from "../../../../services/models/page-response-book-response";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {BookCardComponent} from "../../components/book-card/book-card.component";
+import {BookResponse} from "../../../../services/models/book-response";
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [
     NgForOf,
-    BookCardComponent
+    BookCardComponent,
+    NgIf
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
@@ -20,6 +22,8 @@ export class BookListComponent implements OnInit{
   page = 0;
   size = 2;
   pages: any = [];
+  message = '';
+  level: 'success' |'error' = 'success';
   constructor(
     private bookService: BookService,
     private router: Router
@@ -68,5 +72,23 @@ export class BookListComponent implements OnInit{
 
   get isLastPage() {
     return this.page === this.bookResponse.totalPages as number - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.level = 'success';
+    this.bookService.borrowBook({
+      'book-id': book.id as number
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Livre ajouté avec succès à votre liste';
+      },
+      error: (err) => {
+        console.log(err);
+        this.level = 'error';
+        this.message = err.error.error;
+      }
+    });
   }
 }
