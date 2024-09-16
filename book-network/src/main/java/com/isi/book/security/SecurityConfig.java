c6a7f0ae4089d8d48d02aa7e68695871ba4f97ed
Abdellah <abdallahkaba98@gmail.com>
2024-09-16 23:33:46 +0000

    configuration-websocket-backend

--- book-network/src/main/java/com/isi/book/security/SecurityConfig.java
+++ book-network/src/main/java/com/isi/book/security/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig {
                                         "/configuration/security",
                                         "/swagger-ui/**",
                                         "/webjars/**",
-                                        "/swagger-ui.html"
+                                        "/swagger-ui.html",
+                                        "/ws/**"
                                 )
                                 .permitAll()
                                 .anyRequest()
